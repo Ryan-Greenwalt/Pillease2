8bed2d3e0f19e53c33b19fee4770be61a9f2a282
Takira <123585167+TakiraB@users.noreply.github.com>
2023-11-17 19:48:36 -0800

    Create Globals.js for global variables (colors/font size)

---
+++ app/components/Globals.js
@@ -0,0 +1,20 @@
+// Globals holds all global styles, more to be added later
+
+import { StyleSheet } from 'react-native';
+
+export default {
+
+    // lavender
+    primary: '#E8DDEA',
+
+    // off-white
+    primaryLight: '#FBFCF8',
+
+    // dark grey
+    navIconDark: '#5B5B5B',
+
+    // light pastel green
+    buttonLight: '#B3DABE',
+
+    
+};
