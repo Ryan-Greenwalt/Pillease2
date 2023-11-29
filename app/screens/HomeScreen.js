7d5e1d9cff0aeffaf425d1bb626030df3984f0db
Takira <123585167+TakiraB@users.noreply.github.com>
2023-11-28 17:07:38 -0800

    added function to handle photo uploads + button

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { StyleSheet, Button, Text, View , Image} from 'react-native';
 import Medication from '../components/Medication';
+import * as ImagePicker from 'expo-image-picker';
 
 function HomeScreen({ navigation, route }) {
     const [medications, setMedications] = React.useState([]);
+    const [userName, setUserName] = useState('User');
+    const [userPhoto, setUserPhoto] = useState(null);
 
     React.useEffect(() => {
         if(route.params?.medName) {
@@ -34,18 +38,38 @@ function HomeScreen({ navigation, route }) {
         setMedications(newMeds);
     }
 
-    return (
+    const handleUploadPhoto = async () => {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    
+        if (status !== 'granted') {
+          console.log('Permission to access media library denied');
+          return;
+        }
+    
+        const result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        if (!result.canceled) {
+          setUserPhoto(result.uri);
+        }
+      };
+
+
+      return (
         <View style={styles.container}>
-            {medications.map((medication, index) => (
-                <Medication medInfo={medication} key={index} onDelete={handleDeleteMed} />
-            ))}
-            <Button
-                title="New Med"
-                onPress={handleNewMed}
-            />
+          {userPhoto && <Image source={{ uri: userPhoto }} style={styles.userPhoto} />}
+          {medications.map((medication, index) => (
+            <Medication medInfo={medication} key={index} onDelete={handleDeleteMed} />
+          ))}
+          <Button title="New Med" onPress={handleNewMed} />
+          <Button title="Upload Photo" onPress={handleUploadPhoto} />
         </View>
-    );
-}
+      );
+    }
 
 const styles = StyleSheet.create({
     container: {
