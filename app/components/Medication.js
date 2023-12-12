0c22163a5feb6cb5a1e54f961551b04b86e3fd94
Takira <123585167+TakiraB@users.noreply.github.com>
2023-12-12 10:40:22 -0800

    Added styling to Medication info

--- app/components/Medication.js
+++ app/components/Medication.js
@@ -1,49 +1,59 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-// npx expo install expo-checkbox
 import CheckBox from 'expo-checkbox';
 
-function Medication({medInfo: { medName, dosageAmount, dosageUnit, hourToTake, minuteToTake, amPm, days, medNumber }, onDelete, onEdit}) {
-    const [taken, setTaken] = React.useState(false);
+function Medication({ medInfo: { medName, dosageAmount, dosageUnit, hourToTake, minuteToTake, amPm, days, medNumber }, onDelete, onEdit }) {
+  const [taken, setTaken] = React.useState(false);
 
-    handleDeleteMed = () => {
-        onDelete?.(medNumber);
-    }
+  const handleDeleteMed = () => {
+    onDelete?.(medNumber);
+  };
 
-    handleEditMed = () => {
-        onEdit?.(medName, dosageAmount, dosageUnit, hourToTake, minuteToTake, amPm, days, medNumber)
-    }
+  const handleEditMed = () => {
+    onEdit?.(medName, dosageAmount, dosageUnit, hourToTake, minuteToTake, amPm, days, medNumber);
+  };
 
-    handleCheckBox = () => {
-        setTaken(!taken);
-    }
+  const handleCheckBox = () => {
+    setTaken(!taken);
+  };
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.medNameRow}>
-                <Text style={styles.medName}>{medName}</Text>
-                <CheckBox
-                    value={taken}
-                    onValueChange={handleCheckBox}
-                />
-            </View>
-            <Text>Dosage: {dosageAmount} {dosageUnit}</Text>
-            <Text>Take at: {hourToTake}:{minuteToTake} {amPm}</Text>
-            <Text>{days.join(', ')}</Text>
-            <View style={styles.buttonContainer}>
-            <Button
-                title="Edit"
-                onPress={handleEditMed}
-                style={styles.button}
-            />
-            <Button
-                title="Delete"
-                onPress={handleDeleteMed}
-                style={styles.button}
-            />
-            </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.medicationInfoContainer}>
+        <View style={styles.medNameRow}>
+          <Text style={styles.boldText}>{medName}</Text>
+          <CheckBox
+            value={taken}
+            onValueChange={handleCheckBox}
+          />
         </View>
-    );
+        <View style={styles.infoRow}>
+          <Text style={styles.label}>Dosage:</Text>
+          <Text>{dosageAmount} {dosageUnit}</Text>
+        </View>
+        <View style={styles.infoRow}>
+          <Text style={styles.label}>Take at:</Text>
+          <Text>{hourToTake}:{minuteToTake} {amPm}</Text>
+        </View>
+        <View style={styles.infoRow}>
+          <Text style={styles.label}>Days:</Text>
+          <Text>{days.join(', ')}</Text>
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Edit"
+          onPress={handleEditMed}
+          style={styles.button}
+        />
+        <Button
+          title="Delete"
+          onPress={handleDeleteMed}
+          style={styles.button}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -54,22 +64,39 @@ const styles = StyleSheet.create({
     width: '80%',
     marginTop: 10,
     marginBottom: 10,
+    borderRadius: 10,
   },
-  medName: {
-    fontWeight: 'bold'
+  boldText: {
+    fontWeight: 'bold',
+    marginRight: 5,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginRight: 5,
   },
   buttonContainer: {
     flexDirection: 'row',
     marginTop: 10,
     justifyContent: 'space-between',
-    width: '75%', 
+    width: '75%',
   },
   button: {
-    marginHorizontal: 5, 
+    marginHorizontal: 5,
   },
   medNameRow: {
     flexDirection: 'row',
-  }
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  infoRow: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  medicationInfoContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 5,
+  },
 });
 
 export default Medication;
