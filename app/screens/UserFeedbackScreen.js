fadae5a5f1db4a9aaf464e1143eb4142a8b93313
Takira <123585167+TakiraB@users.noreply.github.com>
2023-12-14 10:21:11 -0800

    styling changes

--- app/screens/UserFeedbackScreen.js
+++ app/screens/UserFeedbackScreen.js
@@ -1,56 +1,73 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight, Alert } from 'react-native';
+import Globals from '../components/Globals';
 
 function UserFeedbackScreen(props) {
-    const [feedback, setFeedback] = useState('');
+  const [feedback, setFeedback] = useState('');
 
-    const handleFeedbackSubmit = () => {
-    // additional logic can go here if we want to actually submit feedback
-        Alert.alert('Feedback Submitted', feedback);
-    };
+  const handleFeedbackSubmit = () => {
+    // Implement your logic to handle user feedback here
+    Alert.alert('Feedback Submitted', feedback);
+  };
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.heading}>Have Advice? Let Us Know!</Text>
-            
-            {/* Feedback Input */}
-            <TextInput
-                style={styles.input}
-                multiline
-                placeholder="Type your feedback here..."
-                value={feedback}
-                onChangeText={text => setFeedback(text)}
-            />
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Have Advice? Let Us Know!</Text>
 
-            {/* Submit Button */}
-            <Button
-                title="Submit Feedback"
-                onPress={handleFeedbackSubmit}
-            />
-        </View>
-    );
+      {/* Feedback Input */}
+      <TextInput
+        style={styles.input}
+        multiline
+        placeholder="Type your feedback here..."
+        value={feedback}
+        onChangeText={(text) => setFeedback(text)}
+      />
+
+      {/* Submit Button */}
+      <TouchableHighlight
+        style={styles.button}
+        underlayColor="#DDDDDD"
+        onPress={handleFeedbackSubmit}
+      >
+        <Text style={styles.buttonText}>Submit Feedback</Text>
+      </TouchableHighlight>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 16,
-    },
-    heading: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 16,
-    },
-    input: {
-        height: 120,
-        width: '100%',
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginBottom: 16,
-        padding: 8,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input: {
+    height: 120,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 8,
+  },
+  button: {
+    backgroundColor: Globals.buttonLight, 
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 20,
+    marginBottom: 15,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });
 
 export default UserFeedbackScreen;
