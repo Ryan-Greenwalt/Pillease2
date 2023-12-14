421b33cb141d1887c5e259f1355adb1d7087242e
Takira <123585167+TakiraB@users.noreply.github.com>
2023-12-14 10:17:47 -0800

    Added WIP picture + styling

--- app/screens/HealthProfileScreen.js
+++ app/screens/HealthProfileScreen.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 function HealthProfileScreen(props) {
-    return (
-        <View style={styles}>
-            <Text>Health Profile</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/WIP.png')} style={styles.image} />
+      <Text style={styles.message}>Under construction, come back soon!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-})
+    alignItems: 'center',
+  },
+  image: {
+    width: 200, 
+    height: 200, 
+    resizeMode: 'contain', 
+  },
+  message: {
+    marginTop: 20, 
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default HealthProfileScreen;
